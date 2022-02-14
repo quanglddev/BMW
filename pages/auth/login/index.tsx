c4bf4898b10a136f187b3a19a8ae96a98d2563d3
Quang Luong <quanglddev@gmail.com>
2022-02-14 01:36:02 -0500

    implement new color theme

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -15,97 +15,61 @@ import {
 } from "@firebase/firestore";
 import backgroundPic from "../../../public/background_mobile.jpeg";
 import ResponsiveAppBar from "../../../components/ResponsiveAppBar";
+import Logo from "../../../public/icons/logo.svg";
+import Censored from "../../../public/icons/censored.svg";
+import Uncensored from "../../../public/icons/uncensored.svg";
+import Facebook from "../../../public/icons/facebook.svg";
+import Google from "../../../public/icons/google.svg";
 
 const Login: NextPage = () => {
   const [censorPassword, setCensorPassword] = useState<boolean>(true);
 
   return (
-    <div className="relative flex w-screen h-screen flex-col items-center bg-navigation-bar">
-      <div className="flex flex-row items-center -mt-1 justify-center w-full">
-        <svg
-          className="fill-current w-16 h-16"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-        >
-          <path
-            fill="#000"
-            d="M34.476,24c0-5.787-4.69-10.476-10.476-10.476S13.524,18.213,13.524,24S18.213,34.476,24,34.476S34.476,29.787,34.476,24L34.476,24z"
-          />
-          <path
-            fill="#679343"
-            d="M24 14c-5.459.141-9.859 4.542-10 10h10V14L24 14zM24 34.002c5.459-.143 9.859-4.544 10-10.002H24V34.002L24 34.002z"
-          />
-          <path
-            fill="#000"
-            d="M24,35c-6.066,0-11-4.934-11-11s4.934-11,11-11s11,4.934,11,11S30.066,35,24,35z M24,15c-4.963,0-9,4.037-9,9s4.037,9,9,9c4.962,0,9-4.037,9-9S28.962,15,24,15z"
-          />
-        </svg>
-        <div className="-ml-3 text-lg text-white">BMWordle</div>
+    <div className="relative flex w-screen h-screen flex-col items-center">
+      <div className="flex flex-row items-center -mt-1 justify-center w-full drop-shadow-lg bg-red-dark-99">
+        <Logo className="fill-current w-16 h-16 -ml-6"></Logo>
+        <div className="-ml-3 text-lg font-bold text-white">BMWordle</div>
       </div>
 
       {/* Form */}
-      <div className="flex w-full h-full flex-col items-center z-10">
-        <form className="flex flex-col items-center bg-form-1 w-10/12 p-2 py-5 rounded-xl">
+      <div className="flex w-full h-full flex-col items-center py-8 bg-gradient-3">
+        <form className="flex flex-col items-center bg-white w-10/12 p-2 py-5 rounded-xl drop-shadow-lg">
           <input
             type="text"
-            className="bg-text-field-1 text-icon rounded-sm p-2 text-sm w-11/12 border-2 border-btn-1"
+            className="text-black rounded-sm p-2 w-11/12 drop-shadow-md bg-pink-light-1"
             placeholder="Username or Email"
           />
 
           <div className="flex flex-row w-11/12 items-center mt-3 relative">
             <input
               type={`${censorPassword ? "password" : "text"}`}
-              className="bg-text-field-1 text-icon rounded-sm p-2 text-sm w-full border-2 border-btn-1"
+              className="text-black rounded-sm p-2 w-full drop-shadow-md bg-pink-light-1"
               placeholder="Password"
             />
             {!censorPassword ? (
-              <svg
-                className="fill-current text-green-1 h-5 w-5 absolute right-3"
-                viewBox="0 0 48 48"
+              <Censored
+                className="fill-current h-5 w-5 absolute right-3"
                 onClick={() => setCensorPassword(!censorPassword)}
-              >
-                <path
-                  fill="#3d3b39"
-                  d="M5.995,24c0,0,8.06-11,18.004-11s17.996,11,17.996,11S33.944,35,24,35S5.995,24,5.995,24z"
-                />
-                <path d="M24,13c9.945,0,17.996,11,17.996,11S33.944,35,24,35S5.995,24,5.995,24S14.055,13,24,13 M24,11C13.165,11,4.735,22.335,4.382,22.818L3.516,24l0.866,1.182C4.735,25.665,13.165,37,24,37s19.256-11.336,19.61-11.818L44.474,24l-0.864-1.181C43.256,22.336,34.834,11,24,11L24,11z" />
-                <path d="M15,23.953c0-4.971,4.034-8.994,9-8.994c4.967,0,9,4.023,9,8.994c0,4.977-4.033,9.006-9,9.006C19.034,32.959,15,28.93,15,23.953z" />
-                <path d="M20,23.958c0-2.213,1.786-3.999,4-3.999c2.203,0,4,1.786,4,3.999c0,2.22-1.797,4.001-4,4.001C21.786,27.959,20,26.178,20,23.958z" />
-                <path d="M6.025,23.96C6.549,23.26,14.394,13,24,13c9.606,0,17.442,10.26,17.965,10.96h4.958l-1.7-2.322C44.846,21.121,35.838,9,24,9C12.163,9,3.146,21.12,2.769,21.636L1.065,23.96H6.025z" />
-                <path
-                  d="M21.586 -3.284H25.586V50.456H21.586z"
-                  transform="rotate(-45.001 23.586 23.586)"
-                />
-              </svg>
+              ></Censored>
             ) : (
-              <svg
-                className="fill-current text-green-1 h-5 w-5 absolute right-3"
-                viewBox="0 0 48 48"
+              <Uncensored
+                className="fill-current h-5 w-5 absolute right-3"
                 onClick={() => setCensorPassword(!censorPassword)}
-              >
-                <path
-                  fill="#3d3b39"
-                  d="M5.93,25.041c0,0,8.06-11.041,18.004-11.041S41.93,25.041,41.93,25.041S33.879,36,23.934,36S5.93,25.041,5.93,25.041z"
-                />
-                <path d="M23.934,14c9.945,0,17.996,11.041,17.996,11.041S33.879,36,23.934,36S5.93,25.041,5.93,25.041S13.989,14,23.934,14 M23.934,12C13.098,12,4.668,23.377,4.314,23.862L3.45,25.046l0.868,1.18C4.672,26.707,13.102,38,23.934,38c10.833,0,19.254-11.294,19.607-11.774l0.867-1.18l-0.862-1.183C43.192,23.378,34.771,12,23.934,12L23.934,12z" />
-                <path d="M14.935,24.994c0-4.971,4.034-8.994,9-8.994c4.968,0,9,4.023,9,8.994c0,4.977-4.032,9.006-9,9.006C18.968,34,14.935,29.971,14.935,24.994z" />
-                <path d="M19.935,24.999c0-2.213,1.786-3.999,4-3.999c2.204,0,4,1.786,4,3.999c0,2.22-1.796,4.001-4,4.001C21.721,29,19.935,27.219,19.935,24.999z" />
-                <path d="M5.959,25c0.524-0.7,8.369-10.96,17.975-10.96c9.605,0,17.442,10.259,17.966,10.959L46.857,25l-1.699-2.322c-0.379-0.517-9.387-12.638-21.224-12.638c-11.837,0-20.853,12.12-21.231,12.636L1,25H5.959z" />
-              </svg>
+              ></Uncensored>
             )}
           </div>
 
-          <div className="flex flex-row w-11/12 items-center justify-between mt-2">
-            <div className="text-icon text-xs">Forgot Password?</div>
+          <div className="flex flex-row w-11/12 items-center justify-between mt-3">
+            <div className="text-gray-700 text-xs">Forgot Password?</div>
             <div className="flex flex-row items-center">
               <input className="text-icon text-xs" type="checkbox" />
-              <div className="text-icon text-xs ml-2">Remember</div>
+              <div className="text-gray-700 text-xs ml-2">Remember</div>
             </div>
           </div>
 
           <button
             type="submit"
-            className="flex flex-row w-11/12 h-16 bg-button-2 rounded-xl mt-5 items-center border-b-4 border-btn justify-center"
+            className="flex flex-row w-11/12 h-16 rounded-xl mt-5 items-center border-b-4 justify-center border-red-800 bg-red-dark-99"
           >
             <div className="font-bold text-white text-2xl">Log In</div>
           </button>
@@ -114,50 +78,24 @@ const Login: NextPage = () => {
             className="flex items-center justify-center w-11/12 bg-text-field-1 mt-6 relative"
             style={{ height: "1px" }}
           >
-            <div className="absolute text-xs text-icon bg-form-1 px-3">
+            <div className="absolute text-xs text-gray-700 bg-white px-3">
               or connect with
             </div>
           </div>
 
           <div className="flex flex-row items-center mt-5">
-            <svg className="fill-current w-6 h-6" viewBox="0 0 48 48">
-              <path
-                fill="#217CEF"
-                d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"
-              />
-              <path
-                fill="#fff"
-                d="M29.368,24H26v12h-5V24h-3v-4h3v-2.41c0.002-3.508,1.459-5.59,5.592-5.59H30v4h-2.287 C26.104,16,26,16.6,26,17.723V20h4L29.368,24z"
-              />
-            </svg>
+            <Facebook className="fill-current w-6 h-6"></Facebook>
             <div className="ml-2 text-facebook text-sm font-bold">Facebook</div>
           </div>
 
           <div className="flex flex-row items-center mt-3">
-            <svg className="fill-current w-6 h-6" viewBox="0 0 48 48">
-              <path
-                fill="#FFC107"
-                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
-              />
-              <path
-                fill="#FF3D00"
-                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
-              />
-              <path
-                fill="#4CAF50"
-                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
-              />
-              <path
-                fill="#1976D2"
-                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
-              />
-            </svg>
+            <Google className="fill-current w-6 h-6"></Google>
             <div className="ml-2 text-google text-sm font-bold">Google</div>
           </div>
 
           <div className="flex flex-row items-center mt-3">
-            <div className="text-sm text-gray-400">New?</div>
-            <div className="text-sm ml-2 text-blue-400">
+            <div className="text-sm text-gray-700">New?</div>
+            <div className="text-sm ml-2 font-semibold text-red-dark-99">
               <Link href="/auth/signup">
                 <a>Sign up - it&apos;s FREE!</a>
               </Link>
--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -12,71 +12,74 @@ import {
   getDocs,
   onSnapshot,
 } from "@firebase/firestore";
-import backgroundPic from "../../../public/background_mobile.jpeg";
+import Censored from "../../../public/icons/censored.svg";
+import Uncensored from "../../../public/icons/uncensored.svg";
 import ResponsiveAppBar from "../../../components/ResponsiveAppBar";
 
 const SignUp: NextPage = () => {
+  const [censorPassword, setCensorPassword] = useState<boolean>(true);
+
   return (
-    <div className="relative flex w-screen h-screen flex-col items-center bg-navigation-bar">
+    <div className="relative flex w-screen h-screen flex-col items-center">
       <div className="z-50">
         <ResponsiveAppBar></ResponsiveAppBar>
       </div>
 
       <div className="flex w-full h-full flex-col items-center z-10 mt-12">
-        <div className="text-white font-semibold text-3xl mx-4 text-center -mt-8">
+        <div className="text-black font-semibold text-3xl mx-4 text-center -mt-4">
           <span>Join Now - It&apos;s Free &#38;</span> <span>Easy!</span>
         </div>
 
         {/* Form */}
         <div className="flex w-full h-full flex-col items-center z-10 mt-5">
           <form className="flex flex-col items-center w-11/12 rounded-xl">
-            <div className="text-xs text-icon w-11/12 font-bold">Username</div>
+            <div className="text-xs text-gray-700 w-11/12 font-bold">
+              Username
+            </div>
             <input
               type="text"
-              className="bg-text-field-1 text-icon rounded-sm p-2 text-sm w-11/12 border-2 border-btn-1"
+              className="text-black rounded-sm p-2 w-11/12 drop-shadow-md bg-pink-light-1"
             />
 
-            <div className="text-xs text-icon w-11/12 font-bold mt-3">
+            <div className="text-xs text-gray-700 w-11/12 font-bold mt-3">
               Email
             </div>
             <input
               type="text"
-              className="bg-text-field-1 text-icon rounded-sm p-2 text-sm w-11/12 border-2 border-btn-1"
+              className="text-black rounded-sm p-2 w-11/12 drop-shadow-md bg-pink-light-1"
             />
 
-            <div className="text-xs text-icon w-11/12 font-bold mt-3">
+            <div className="text-xs text-gray-700 w-11/12 font-bold mt-3">
               Password
             </div>
             <div className="flex flex-row w-11/12 items-center relative">
               <input
                 type="text"
-                className="bg-text-field-1 text-icon rounded-sm p-2 text-sm w-full border-2 border-btn-1"
+                className="text-black rounded-sm p-2 w-full drop-shadow-md bg-pink-light-1"
               />
-              <svg
-                className="fill-current text-green-1 h-5 w-5 absolute right-3"
-                viewBox="0 0 48 48"
-              >
-                <path
-                  fill="#3d3b39"
-                  d="M5.93,25.041c0,0,8.06-11.041,18.004-11.041S41.93,25.041,41.93,25.041S33.879,36,23.934,36S5.93,25.041,5.93,25.041z"
-                />
-                <path d="M23.934,14c9.945,0,17.996,11.041,17.996,11.041S33.879,36,23.934,36S5.93,25.041,5.93,25.041S13.989,14,23.934,14 M23.934,12C13.098,12,4.668,23.377,4.314,23.862L3.45,25.046l0.868,1.18C4.672,26.707,13.102,38,23.934,38c10.833,0,19.254-11.294,19.607-11.774l0.867-1.18l-0.862-1.183C43.192,23.378,34.771,12,23.934,12L23.934,12z" />
-                <path d="M14.935,24.994c0-4.971,4.034-8.994,9-8.994c4.968,0,9,4.023,9,8.994c0,4.977-4.032,9.006-9,9.006C18.968,34,14.935,29.971,14.935,24.994z" />
-                <path d="M19.935,24.999c0-2.213,1.786-3.999,4-3.999c2.204,0,4,1.786,4,3.999c0,2.22-1.796,4.001-4,4.001C21.721,29,19.935,27.219,19.935,24.999z" />
-                <path d="M5.959,25c0.524-0.7,8.369-10.96,17.975-10.96c9.605,0,17.442,10.259,17.966,10.959L46.857,25l-1.699-2.322c-0.379-0.517-9.387-12.638-21.224-12.638c-11.837,0-20.853,12.12-21.231,12.636L1,25H5.959z" />
-              </svg>
+              {!censorPassword ? (
+                <Censored
+                  className="fill-current h-5 w-5 absolute right-3"
+                  onClick={() => setCensorPassword(!censorPassword)}
+                ></Censored>
+              ) : (
+                <Uncensored
+                  className="fill-current h-5 w-5 absolute right-3"
+                  onClick={() => setCensorPassword(!censorPassword)}
+                ></Uncensored>
+              )}
             </div>
 
             <button
               type="submit"
-              className="flex flex-row w-11/12 h-12 bg-button-2 rounded-md mt-5 items-center justify-center"
+              className="flex flex-row w-11/12 h-12 bg-red-dark-99 rounded-md mt-5 items-center justify-center"
             >
               <div className="font-bold text-white text-sm">
                 Create Your FREE Account
               </div>
             </button>
 
-            <div className="text-sm text-icon x-3 my-5">
+            <div className="text-sm text-gray-500 x-3 my-5">
               or sign up using...
             </div>
 
