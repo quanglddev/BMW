b38b3849d877be6128452bf0f298a64866d548f3
Quang Luong <quanglddev@gmail.com>
2022-02-14 13:44:45 -0500

    attempt to fix private key issue

--- utils/initAuth.tsx
+++ utils/initAuth.tsx
@@ -19,8 +19,8 @@ const initAuth = () => {
     firebaseClientInitConfig: {
       apiKey: process.env.FIREBASE_API_KEY!,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID
-        ? JSON.parse(process.env.FIREBASE_PROJECT_ID)
+      projectId: process.env.FIREBASE_PRIVATE_KEY
+        ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
         : "",
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
