7d025c46e8fa6bc956ed153c42a9ea297ce30678
Quang Luong <quanglddev@gmail.com>
2022-02-14 16:43:35 -0500

    attempt to fix private key issue

--- utils/initAuth.tsx
+++ utils/initAuth.tsx
@@ -13,8 +13,8 @@ const initAuth = () => {
         projectId: process.env.FIREBASE_PROJECT_ID!,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
         privateKey: process.env.FIREBASE_PRIVATE_KEY
-          ? process.env.FIREBASE_PRIVATE_KEY
-          : "",
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined,
       },
       databaseURL: "https://cs338-bmw.firebaseio.com",
     },
