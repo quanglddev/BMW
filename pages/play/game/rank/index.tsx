bac31e41660e19fbfdd0bf16189dbca10a229f32
Quang Luong <quanglddev@gmail.com>
2022-02-24 21:18:44 -0500

    matchmaking doesn't work

--- pages/play/game/rank/index.tsx
+++ pages/play/game/rank/index.tsx
@@ -50,7 +50,7 @@ const RankMatchmaking: NextPage = () => {
         const data = querySnapshot.docs[0].data();
         const ids = data.ids as string[];
 
-        if (!ids || ids.length === 0) {
+        if (!ids || ids.length <= 1) {
           await joinWaitRoom(AuthUser.id);
         } else if (ids.length > 1) {
           const roomId = await createJointRoom(AuthUser.id, ids);
